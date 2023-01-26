13a3bd57a0f874977bbb14b59ac7a56935e1093c
HenrikDK <commander.john.crichton@gmail.com>
2023-01-26 01:23:50 +0100

    add ui project setup

---
+++ src/Sentinel.Dashboard.Ui/Program.cs
@@ -0,0 +1,54 @@
+using Sentinel.Dashboard.Ui.Model.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseLamar((context, registry) =>
+{
+    registry.Scan(x =>
+    {
+        x.AssemblyContainingType<Program>();
+        x.WithDefaultConventions();
+        x.LookForRegistries();
+    });
+
+    registry.For<IHumioRepository>().Use<FakioRepository>();
+});
+
+builder.WebHost.ConfigureKestrel(x => x.ListenAnyIP(8090))
+    .ConfigureServices(c =>
+    {
+        c.AddMemoryCache();
+        c.AddRazorPages(options =>
+        {
+            options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
+        });
+    })
+    .ConfigureLogging((context, config) =>
+    {
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Warning()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .WriteTo.Console()
+            //.If(Debugger.IsAttached, c => c.WriteTo.Console(), c => c.WriteTo.Console(new JsonFormatter(renderMessage:true)))
+            .CreateLogger();
+
+        config.ClearProviders();
+        config.AddSerilog(logger);
+    });
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapMetrics();
+
+app.Run();
