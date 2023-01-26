edcea69bf2b71e8cb3de1744addc29f736138fd9
HenrikDK <commander.john.crichton@gmail.com>
2023-01-26 01:44:10 +0100

    add test project

---
+++ src/Sentinel.Dashboard.Ui.Test/GlobalUsings.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Linq;
+global using System.IO;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using FluentAssertions;
+global using Lamar;
+global using NUnit.Framework;
