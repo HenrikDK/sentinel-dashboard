87c018ddf4090f2eb0cc9200c67de13b04431bf4
HenrikDK <commander.john.crichton@gmail.com>
2023-07-03 22:26:41 +0200

    add environments query

--- Sentinel.Dashboard.Ui/Model/Repositories/PrometheusRepository.cs
+++ Sentinel.Dashboard.Ui/Model/Repositories/PrometheusRepository.cs
@@ -9,5 +9,34 @@ public interface IPrometheusRepository
 
 public class PrometheusRepository : IPrometheusRepository
 {
+    private readonly IConfiguration _configuration;
+    private Lazy<string> _server;
+    private Lazy<string> _username;
+    private Lazy<string> _password;
+    private Lazy<string> _org;
+
+    public PrometheusRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        
+        _server = new Lazy<string>(() => _configuration.GetValue("prometheus-api", "https://mimir.azure.dsb.dk/prometheus"));
+        _username = new Lazy<string>(() => _configuration.GetValue("prometheus-user", ""));
+        _password = new Lazy<string>(() => _configuration.GetValue("prometheus-pass", ""));
+        _org = new Lazy<string>(() => _configuration.GetValue("prometheus-org", ""));
+    }
+    
+    public string GetEnvironments()
+    {
+        var query = "count by (aks_space, namespace) (up{aks_version!=\"\", aks_space!=\"\"})";
+        
+        var result = _server.Value
+            .AppendPathSegment($"api/v1/query")
+            .WithHeader("X-Scope-OrgId", _org.Value)
+            .SetQueryParam("query", query)
+            .WithBasicAuth(_username.Value, _password.Value)
+            .GetStringAsync().Result;
+
+        return result;
+    }
 
 }
