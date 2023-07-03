0d869b976fc50506e2296beb4757b69ed4a046d0
HenrikDK <commander.john.crichton@gmail.com>
2023-07-03 22:51:45 +0200

    add prometheus repository tests

---
+++ Sentinel.Dashboard.Ui.Test/IntegrationTests/PrometheusRepositoryTests.cs
@@ -0,0 +1,40 @@
+using Sentinel.Dashboard.Ui.Model;
+using Sentinel.Dashboard.Ui.Model.Repositories;
+
+namespace Sentinel.Dashboard.Ui.Test.IntegrationTests;
+
+public class PrometheusRepositoryTests
+{
+    private Container _container;
+    private IConfiguration _configuration;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var registry = new ServiceRegistry();
+        registry.Scan(x =>
+        {
+            x.AssemblyContainingType<IIssuesRepository>();
+            x.WithDefaultConventions();
+            x.LookForRegistries();
+        });
+        registry.AddSingleton(_configuration);
+        registry.AddMemoryCache();
+        _container = new Container(registry);
+    }
+
+    //[Test]
+    public void Should_get_environments_from_config()
+    {
+        var repository = _container.GetInstance<IPrometheusRepository>();
+
+        var environments = repository.GetEnvironments();
+
+        environments.Should().NotBeNullOrEmpty();
+    }
+}
