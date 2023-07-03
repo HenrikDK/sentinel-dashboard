05d9ff9859d488f90a6527ade4dc6e6f83967b57
HenrikDK <commander.john.crichton@gmail.com>
2023-07-03 23:32:35 +0200

    add prometheus endpoints for deployments and alerts

--- Sentinel.Dashboard.Ui/Controllers/ApiController.cs
+++ Sentinel.Dashboard.Ui/Controllers/ApiController.cs
@@ -1,4 +1,4 @@
-using Sentinel.Dashboard.Ui.Model.Repositories;
+﻿using Sentinel.Dashboard.Ui.Model.Repositories;
 
 namespace Sentinel.Dashboard.Ui.Controllers;
 
@@ -12,6 +12,18 @@ public class ApiController : ControllerBase
         _prometheusRepository = prometheusRepository;
     }
 
+    [HttpGet("api/deployments")]
+    public ContentResult GetDeployments()
+    {
+        var result = _prometheusRepository.GetDeployments();
+
+        return new ContentResult
+        {
+            Content = result,
+            ContentType = "application/json"
+        };
+    }
+    
     [HttpGet("api/quote")]
     public ContentResult OnGetQuote()
     {
@@ -29,6 +41,18 @@ public class ApiController : ControllerBase
     public ContentResult OnGetEnvironments()
     {
         var result = _prometheusRepository.GetEnvironments();
+        
+        return new ContentResult
+        {
+            Content = result,
+            ContentType = "application/json"
+        };
+    }
+    
+    [HttpGet("api/alerts")]
+    public ContentResult OnGetAlerts()
+    {
+        var result = _prometheusRepository.GetAlerts();
         
         return new ContentResult
         {
