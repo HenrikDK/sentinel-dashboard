4b3ab062adb0aeae39a73f08a4d132355a77f86b
HenrikDK <commander.john.crichton@gmail.com>
2024-04-28 16:45:40 +0200

    update global using

--- Sentinel.Dashboard.Ui/GlobalUsings.cs
+++ Sentinel.Dashboard.Ui/GlobalUsings.cs
@@ -1,9 +1,13 @@
 global using System;
+global using System.Diagnostics;
 global using System.Linq;
 global using System.Numerics;
 global using System.Collections.Generic;
 global using System.Security.Cryptography;
 global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Web;
 
 global using Azure.Extensions.AspNetCore.Configuration.Secrets;
 global using Azure.Identity;
@@ -12,12 +16,15 @@ global using Azure.Security.KeyVault.Secrets;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
 global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
 global using Flurl;
 global using Flurl.Http;
+global using Flurl.Http.Configuration;
+global using Lamar;
 global using Lamar.Microsoft.DependencyInjection;
 global using MoreLinq.Extensions;
 global using Prometheus;
